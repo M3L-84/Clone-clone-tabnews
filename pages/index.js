1feaaf8f6c5923b7c8e1c6a4344bc4d774a53010
M3L <manu.tec@outlook.com>
2026-02-20 11:02:53 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <h1>
+      Meu amor, eu amo muito você e irei lutar com todas as minhas forças e não
+      desistirei!
+    </h1>
+  );
+}
+
+export default Home;
